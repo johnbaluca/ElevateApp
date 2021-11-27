321c5d71b137c26835129856542ae9e9e0944b01
Gerard Nicolas <gerard.nicolas@benilde.edu.ph>
2021-11-27 17:09:56 +0800

    Landscape Variations, Share Score, Fixed Design

--- app/src/main/java/com/example/elevatemobdevt/ScoreActivity.kt
+++ app/src/main/java/com/example/elevatemobdevt/ScoreActivity.kt
@@ -3,8 +3,11 @@ package com.example.elevatemobdevt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_score.*
+import java.util.*
 
 
 class ScoreActivity : AppCompatActivity() {
@@ -28,6 +31,31 @@ class ScoreActivity : AppCompatActivity() {
         elevatescorebtn_finish.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_game, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.itemShare){
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Elevate App Quiz Game:" +
+                "\nYou got a score of ${getCorrectAnswers().toString()} out of 10" +
+                "\non ${Calendar.getInstance().time}")
+                type = "text/plain"
+            }
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getCorrectAnswers(): Int{
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        return correctAnswers
+    }
+
 }
